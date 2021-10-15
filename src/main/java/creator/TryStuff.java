f282f624942b531e662e01fd8df941a0a2c9777e
Kris <kristof.solak@gmail.com>
2021-10-15 15:28:10 +0200

    added try

---
+++ src/main/java/creator/TryStuff.java
@@ -0,0 +1,7 @@
+package creator;
+
+public class TryStuff {
+    public static int addTwoNum(int a, int b){
+        return a + b;
+    }
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,9 @@
+import creator.TryStuff;
+
 public class main {
 
     public static void main(String[] args) {
         System.out.println("asd");
+        TryStuff.addTwoNum(2,6);
     }
 }
