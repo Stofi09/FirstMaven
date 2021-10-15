c6ae415bf1fc19e2b21fcecb955ad5c685ddd2af
Kris <kristof.solak@gmail.com>
2021-10-15 15:38:23 +0200

    src changed

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,7 +3,7 @@ import creator.TryStuff;
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("asd");
+        System.out.println("asdasdasdd");
         TryStuff.addTwoNum(2,6);
     }
 }
