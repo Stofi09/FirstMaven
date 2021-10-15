8b8dc4bbbeb7ef82d7c50ea73d8268d5195e674e
Kris <kristof.solak@gmail.com>
2021-10-15 15:32:22 +0200

    first test

---
+++ src/test/java/TryStuffTest.java
@@ -0,0 +1,11 @@
+import creator.TryStuff;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TryStuffTest {
+
+    @Test
+    public void tryTest(){
+        assertEquals("asd",5, TryStuff.addTwoNum(2,3));
+    }
+}
