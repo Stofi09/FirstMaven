77fe3a4a27b764d2c2ce6693d26a8db3ff4f090c
Kris <kristof.solak@gmail.com>
2021-10-19 13:07:08 +0200

    surfire added

--- src/test/java/CucumberTest.java
+++ src/test/java/CucumberTest.java
@@ -2,15 +2,16 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import library.Library;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 public class CucumberTest {
 
 
     public Library library;
-    public String result;
-
-
+    public String result = "";
+    public Library library2;
+    public String result2 = "";
     @Given(": My library is called {string}")
     public void my_library_is_called(String string) {
        library = new Library(string,"address");
@@ -22,9 +23,25 @@ public class CucumberTest {
             result = "Yes";
         }
     }
-
     @Then(": I should say {string}")
     public void i_should_say(String string) {
       assertEquals(string,result);
+    }
+
+
+    @Given(": My library is in {string}")
+    public void my_library_is_in(String string){
+        library2 = new Library("name",string);
+    }
+    @When(": somebody asks whether my library is in {string}")
+    public void somebody_asks_whether_my_library_is_in(String string){
+        if (string.equalsIgnoreCase(library2.getAddress())){
+            result2 = "Yes";
+        }
+    }
+
+    @Then(": I should say to him {string}")
+    public void i_should_say_to_him(String string){
+        assertEquals(string,result2);
     }
 }
--- target/test-classes/features/Library.feature
+++ target/test-classes/features/Library.feature
@@ -4,3 +4,9 @@ Feature: Test my library
         Given : My library is called "myLib"
         When  : When somebody asks "myLib" is my library
         Then  : I should say "Yes"
+
+
+      Scenario: Where is my library
+        Given : My library is in "Edinburgh"
+        When  : somebody asks whether my library is in "Edinburgh"
+        Then  : I should say to him "Yes"
